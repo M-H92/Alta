f20e2f1d0e045448e29ffbabd7386528e3a0a475
Marc-Henry Sana <mhsana@biaclog.lu>
2024-05-24 13:09:37 +0200

    initial commit

---
+++ alta/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <the-nav-bar />
+  </div>
+</template>
+<script setup>
+import TheNavBar from "~/src/pages/theNavBar/theNavBar.vue";
+import "~/assets/css/reset.css";
+import "~/assets/css/style.css";
+</script>
---
+++ alta/src/pages/theNavBar/theNavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav id="the-nav-bar">
+    <ul>
+      <li v-for="lien in liens">
+        <a href="#">{{ lien.title }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script setup>
+const liens = [
+  {
+    title: "Placeholder",
+    summary: "Placeholder summary",
+    route: "",
+  },
+  {
+    title: "Activité professionnelle",
+    summary: "Placeholder summary",
+    route: "",
+  },
+  {
+    title: "Ma formation",
+    summary: "formation summary",
+    route: "",
+  },
+  {
+    title: "Mes projets",
+    summary: "projets summary",
+    route: "",
+  },
+];
+</script>
