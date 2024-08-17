cf578296738b2ca7708d3d2a6fa6e96818bd9a54
Alta <marc_henry_sana@msn.com>
2024-08-17 20:01:28 +0200

    general

--- alta/app.vue
+++ alta/app.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <the-nav-bar />
+    <!-- <the-nav-bar /> -->
+    <sgbd />
   </div>
 </template>
 <script setup>
-import TheNavBar from "~/src/pages/theNavBar/theNavBar.vue";
+// import TheNavBar from "~/src/pages/theNavBar/theNavBar.vue";
+import sgbd from "~/src/pages/sgbd/sgbd.vue";
 import "~/assets/css/reset.css";
 import "~/assets/css/style.css";
 </script>
