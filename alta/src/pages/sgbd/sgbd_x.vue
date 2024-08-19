b5bb53a916ce21530b0a6c10718bbdb120e7b6db
Marc-Henry Sana <mhsana@biaclog.lu>
2024-08-19 11:54:45 +0200

    db wip index

---
+++ alta/src/pages/sgbd/sgbd_x.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Liste de liens et références externes</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>Site web</th>
+        <th>Page</th>
+        <th>Vu dans</th>
+        <th>Lien</th>
+        <th>Commentaire</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>Site officiel postgresql</th>
+        <th>Page de téléchargement</th>
+        <th>Préambule</th>
+        <th>https://www.postgresql.org/download/</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th>Documentation officielle postgresql</th>
+        <th>Data type</th>
+        <th>Première création d'une DB</th>
+        <th>https://www.postgresql.org/docs/current/datatype.html</th>
+        <th></th>
+      </tr>
+    </tbody>
+  </table>
+</template>
