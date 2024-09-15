5b72eac2b2b0189a8c77c74e7b8ceb5f0a452361
Alta <marc_henry_sana@msn.com>
2024-09-15 21:00:22 +0200

    db - sgdb4

---
+++ alta/src/pages/sgbd/sgbd_4.vue
@@ -0,0 +1,80 @@
+<template>
+  <section>
+    <h2>Battle box bluff</h2>
+    <p>
+      Marabout souhaite éditer une version en ligne de son jeu battle box bluff.
+      <br />
+      Les règles sont les suivantes : <br />
+      Le jeu se joue avec un paquet de 200 cartes <br />
+      Chaque carte contient une affirmation et 3 propositions de réponse<br />
+      2 propositions sont fausses et la dernière est vraie <br />
+      Tour à tour, les joueurs piochent une carte et lise la proposition ainsi
+      qu'une réponse de leur choix <br />
+      Les autres joueurs doivent deviner si oui ou non le joueur qui a tiré la
+      carte a lu le bonne réponse ou a bluffé <br />
+      Chaque carte dispose d'un nombre de point attribué aux joueurs qui ont
+      devinés correctement<br />
+      Chaque carte dispose aussi d'une anecdote liée à la question et d'une
+      catégorie (culture générale, jeu vidéo, géographie,...)<br />
+    </p>
+    <h2>Factures</h2>
+    <p>
+      Un petit indépendant souhaite un programme pour gérer son entreprise<br />
+      Une grande partie de son temps est dédié à la rédaction de devis et au
+      suivi de sa facturation<br />
+      Écrivez une base de données pour un programme qui permettra de créer un
+      devis, une facture liée et de savoir si les factures sont payées ou pas<br />
+      Voici un exemple de devis <br />
+    </p>
+    <div>
+      Devis : 24092005 <br />
+      Client : Thomas George <br />
+      Total TTC : 178.70 € <br />
+      Date : 24/09/20 <br />
+      Accompte : 150 €
+    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Objet</th>
+          <th>Prix unitaire HTVA</th>
+          <th>Unité</th>
+          <th>Quantité</th>
+          <th>Taux de tva</th>
+          <th>Sous-total HTVA</th>
+          <th>Commentaire</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Entretien poele</td>
+          <td>50 €</td>
+          <td>Heure</td>
+          <td>2</td>
+          <td>16%</td>
+          <td>100 €</td>
+          <td>Entretien et changement vitre</td>
+        </tr>
+        <tr>
+          <td>Bois de chauffage</td>
+          <td>6 €</td>
+          <td>Stère</td>
+          <td>10</td>
+          <td>7%</td>
+          <td>60 €</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Vitre poele a bois</td>
+          <td>50 €</td>
+          <td>Pièce</td>
+          <td>1</td>
+          <td>17%</td>
+          <td>50 €</td>
+          <td>En stock</td>
+        </tr>
+      </tbody>
+    </table>
+    <p></p>
+  </section>
+</template>
