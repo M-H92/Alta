6c1b90799af9f974b0c04e4e53889a132dd350ed
Marc-Henry Sana <mhsana@biaclog.lu>
2024-09-23 14:29:18 +0200

    todo et références - mise à jour

---
+++ alta/src/pages/sgbd/todo.vue
@@ -0,0 +1,12 @@
+<template>
+  <ul>
+    <li>
+      Au début de chaque section, script pour avoir une db utilisable
+      correctement
+    </li>
+    <li>section 3, continuer le contenu</li>
+    <li>section 4, modification question battle box (2 à n réponse)</li>
+    <li>section 4, mettre à jour (proposer des solutions)</li>
+    <li>section 5 écrire (many to many et table de liaisons)</li>
+  </ul>
+</template>
