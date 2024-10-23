0c894e1767293d299901b7f069d3c281da3981dd
Marc-Henry Sana <mhsana@biaclog.lu>
2024-10-23 18:01:35 +0200

    try out yaml

--- alta/nuxt.config.ts
+++ alta/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   devtools: { enabled: true },
-  compatibilityDate: "2024-07-16"
-})
+  compatibilityDate: "2024-07-16",
+});
